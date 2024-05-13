6e4b6b9cb6af0e38d76ecd5af2d1e196d3bf3396
Yeosujin88 <yeosujin88@gmail.com>
2024-05-13 10:10:33 +0800

    added basic javascript elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hi");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "hello",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
